0c96fa518fbcc2137884afa7fc5d902b3afe07c6
nasri ahmed <ahmednasri1991@gmail.com>
2022-03-05 18:54:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('Data/Data.csv')
+print(data)
+df = pd.DataFrame(data=data,columns=['Incident ID','Status','Affected CI','Primary Affected Service','Title','Assignment Group','Open Time','Area','Outage Start','Outage End','Assignee','Closure Code','Solution','Description','Department'])
+
+df.drop(['Department','Description','Assignee','Status','Solution'],1,inplace=True)
+print(df)
+
+df.drop(df.loc[df['Closure Code']=='Incident Rejected'].index, inplace=True)
+df.drop(df.loc[df['Area']=='Opération Programée'].index, inplace=True)
+print(df)
+
+#df= df[['Outage End']] - df[['Outage Start']]
+df['Down Time'] = pd.to_datetime(df['Outage End']) - pd.to_datetime(df['Outage Start'])
+
+print(df)
+
+data_2 = pd.read_csv('Data/Data_2.csv')
+print(data_2)
+data_final= pd.merge(data,data_2,on='Affected CI')
+print(data_final)
