d18c5e46ecc95b0d59fdd3fd954e3f7bc1d6dbfa
nasri ahmed <ahmednasri1991@gmail.com>
2022-03-05 19:08:49 +0100

    first commit

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-data = pd.read_csv('Data/Data.csv')
-print(data)
+url = "https://raw.githubusercontent.com/nasriAhmed/DataCleaningPorject/master/Data/data.csv"
+data = pd.read_csv(url)
 df = pd.DataFrame(data=data,columns=['Incident ID','Status','Affected CI','Primary Affected Service','Title','Assignment Group','Open Time','Area','Outage Start','Outage End','Assignee','Closure Code','Solution','Description','Department'])
 
 df.drop(['Department','Description','Assignee','Status','Solution'],1,inplace=True)
@@ -18,8 +17,8 @@ print(df)
 df['Down Time'] = pd.to_datetime(df['Outage End']) - pd.to_datetime(df['Outage Start'])
 
 print(df)
-
-data_2 = pd.read_csv('Data/Data_2.csv')
+urll = "https://raw.githubusercontent.com/nasriAhmed/DataCleaningPorject/master/Data/data_2.csv"
+data_2 = pd.read_csv(urll)
 print(data_2)
-data_final= pd.merge(data,data_2,on='Affected CI')
-print(data_final)
+#data_final= pd.merge(data,data_2,on='Affected CI')
+#print(data_final)
